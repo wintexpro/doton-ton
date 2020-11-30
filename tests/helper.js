2e30d6096a23742fd68201c4c8f3111fc93c13cf
denis <d.kezin@wintex.ru>
2020-11-30 14:56:40 +0700

    update helper, remove deprecated test file

--- tests/helper.js
+++ tests/helper.js
@@ -2,14 +2,25 @@
  * Helpук functions for tests
  */
 
-
-function fromHexWith0x(_string) {
+function fromHexWith0x (_string) {
   return Buffer.from(_string.substring(2), 'hex').toString('utf8')
 }
 
-function toHex(_string, isWith0x = true) {
+function toHex (_string, isWith0x = true) {
   return (isWith0x ? '0x' : '') + Buffer.from(_string, 'utf8').toString('hex')
 }
 
-module.exports.fromHexWith0x = fromHexWith0x;
-module.exports.toHex = toHex;
+/** runLocal for contract from manager (contract MUST BE in manager) */
+async function runLocal (_manager, contract, functionName, input) {
+  const runLocalRes = await _manager.client.contracts.runLocal({
+    address: _manager.contracts[contract].address,
+    functionName: functionName,
+    abi: _manager.contracts[contract].contractPackage.abi,
+    input: input
+  }).catch(e => console.log(e))
+  return runLocalRes.output
+}
+
+module.exports.fromHexWith0x = fromHexWith0x
+module.exports.toHex = toHex
+module.exports.runLocal = runLocal
